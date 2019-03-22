fc3919860d4832f8e0cf677b4cc704e3d94e374d
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2019-03-22 03:21:49 -0300

    Tela Login

--- NarutoApp/app/src/main/java/com/example/narutoapp/LoginActivity.kt
+++ NarutoApp/app/src/main/java/com/example/narutoapp/LoginActivity.kt
@@ -5,40 +5,41 @@ import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.os.Build
 import android.support.v4.content.ContextCompat
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import android.content.Intent
+
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColorTo(R.color.colorPrimary)
+            window.setStatusBarColorTo(R.color.colorPrimaryDark)
         }
     }
 
     override fun onResume() {
         super.onResume()
 
-        val et_senha = findViewById<View>(R.id.password) as EditText
-        val senha = et_senha.toString()
-
         btn_login.setOnClickListener {
-            if(senha.equals("123",false)) {
-                Toast.makeText(this, "hello", Toast.LENGTH_LONG).show()
+            if(user.text.toString() == "admin" && password.text.toString() == "123") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
             }else{
-                Toast.makeText(this, "socorro", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Usuário ou Senha inválido", Toast.LENGTH_LONG).show()
             }
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColorTo(R.color.colorPrimary)
+
+        tv_forgot_password.setOnClickListener {
+            val intent = Intent(this, Recuperar::class.java)
+            startActivity(intent)
         }
     }
 
--- NarutoApp/app/src/main/java/com/example/narutoapp/Recuperar.kt
+++ NarutoApp/app/src/main/java/com/example/narutoapp/Recuperar.kt
@@ -3,15 +3,10 @@ package com.example.narutoapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Recuperar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
+        setContentView(R.layout.activity_recuperar)
     }
 }
